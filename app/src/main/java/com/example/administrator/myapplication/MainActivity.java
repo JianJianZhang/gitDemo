62ada47be2568532e966371ade38cc71e143558e
JianJianZhang <694449594@qq.com>
2017-12-22 17:24:04 +0800

    增加加

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvName = findViewById(R.id.tv_name);
+        tvName.setText("测试2");
     }
 }
